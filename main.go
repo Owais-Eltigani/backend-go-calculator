eb9a16252d348fdbfa205600736f9fa013e6bd6b
Owais-Eltijani <ambidextrous.in@gmail.com>
2025-02-24 23:22:41 +0530

    built the middleware and rate limiter

--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
+	"time"
+
+	"golang.org/x/time/rate"
+	_ "golang.org/x/time/rate"
 )
 
 type Operands struct {
@@ -11,20 +16,64 @@ type Operands struct {
 	Operand2 string `json:"b"`
 }
 
+var (
+	// limiter  = rate.NewLimiter(rate.Limit(2), 0) //?	for time interval rate limiting.
+	vistiors = make(map[string]*rate.Limiter)
+	mutex    sync.Mutex
+)
+
+var limiter = rate.NewLimiter(2, 3)
+var visitors = make(map[string]*rate.Limiter, 0)
+
+func checkIpAddressLimit(ip string) *rate.Limiter {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	v, exist := visitors[ip]
+
+	if !exist {
+
+		v = rate.NewLimiter(rate.Limit(3), 2)
+	}
+	return v
+}
+
+func middleware(next http.HandlerFunc) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		ip := r.RemoteAddr
+		limit := checkIpAddressLimit(ip)
+
+		if !limit.Allow() {
+			http.Error(w, "too many requests", http.StatusTooManyRequests)
+			return
+		}
+		next.ServeHTTP(w, r)
+	}
+}
+
 func main() {
 	// only on test.
 	fmt.Println("backend calculator using golan.")
 
 	//
-	server := http.NewServeMux()
+	router := http.NewServeMux()
 	port := ":5000"
 
-	server.HandleFunc("GET /add", Add)
-	server.HandleFunc("/sub", Sub)
-	server.HandleFunc("/multi", Multi)
-	server.HandleFunc("/div", Div)
+	router.HandleFunc("GET /add", Add)
+	router.HandleFunc("GET /sub", Sub)
+	router.HandleFunc("GET /multi", Multi)
+	router.HandleFunc("GET /div", Div)
 
-	fmt.Println("server...")
-	log.Fatal(http.ListenAndServe(port, server))
+	server := &http.Server{
+		Handler:      router,
+		Addr:         port,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	fmt.Println("router...")
+	log.Fatal(server.ListenAndServe())
 
 }
